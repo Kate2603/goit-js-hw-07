49815968bfda7a21d961ff17fad825bd368afb86
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2023-02-08 15:27:07 +0200

    fixed 01-galery and 02-galery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const insertGalleryItems = (string) => {
+    gallery.insertAdjacentHTML("beforeend", string);
+};
+insertGalleryItems(createGalleryItems(galleryItems));
+
+gallery.addEventListener("click", onGalleryClick);
+
+function createGalleryItems(items) {
+    return items.map(item => 
+        `<div class="gallery__item">
+                <a class="gallery__link" href=${item.original}>
+                <img
+                    class="gallery__image"
+                    src=${item.preview}
+                    data-source=${item.original}
+                    alt=${item.description}
+                /></a>
+            </div>`).join("");
+}
+
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src=${evt.target.dataset.source} width="800" height="600" />`,
+    {
+        onShow: instance => {
+        window.addEventListener('keydown', closeModal);
+        },
+        onClose: instance => {
+        window.removeEventListener('keydown', closeModal);
+        },
+        });
+    
+    function closeModal(evt) {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    }
+    instance.show();
+}
